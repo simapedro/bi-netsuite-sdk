578a7028af93be198433b0382bad359f3f191827
Simão Vale Moreira <smoreira@broadvoice.com>
2025-01-28 10:03:30 +0000

    update currency endpoint

--- netsuitesdk/api/currencyRates.py
+++ netsuitesdk/api/currencyRates.py
@@ -8,3 +8,6 @@ class CurrencyRates(ApiBase):
     def __init__(self, ns_client):
         ApiBase.__init__(self, ns_client=ns_client, type_name='CurrencyRate')
 
+    def get_all(self):
+        return self._get_all()
+
