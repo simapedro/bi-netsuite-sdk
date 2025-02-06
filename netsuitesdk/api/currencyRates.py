28c3739be9798c9aa00d096cec0cde029a215e0a
Simão Vale Moreira <smoreira@broadvoice.com>
2025-02-06 11:31:20 +0000

    update CurrencyRate endpoint

--- netsuitesdk/api/currencyRates.py
+++ netsuitesdk/api/currencyRates.py
@@ -8,9 +8,6 @@ class CurrencyRates(ApiBase):
     def __init__(self, ns_client):
         ApiBase.__init__(self, ns_client=ns_client, type_name='getCurrencyRate')
 
-    def get_all(self):
-        return self._get_all()
-
     def get_all_generator(self, page_size=20):
         """
         Returns a generator which is more efficient memory-wise
