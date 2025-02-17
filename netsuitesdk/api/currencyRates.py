eb800035acee5a98af72769220d9f968367f80c1
Simão Vale Moreira <smoreira@broadvoice.com>
2025-02-17 11:31:01 +0000

    update currencyRates

--- netsuitesdk/api/currencyRates.py
+++ netsuitesdk/api/currencyRates.py
@@ -9,7 +9,7 @@ class CurrencyRates(ApiBase):
     def __init__(self, ns_client):
         ApiBase.__init__(self, ns_client=ns_client, type_name='CurrencyRate')
 
-    def search_currency_rates(self, base_currency='USD', transaction_currency='EUR', effective_date='2025-02-02'):
+    def search_currency_rates(self, base_currency='USD', transaction_currency='EUR', effective_date='2/17/2025'):
         """
         Returns a generator which is more efficient memory-wise
         """
